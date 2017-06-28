638e548a04eda943050b961ef0f2704940a8e7d2
Wagnall <lol95371@pc0088.cs.diamond.ac.uk>
2017-06-28 17:01:17 +0100

    add counter.js, update main.js and index.html

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Counter extends React.component {
+
+constructor() {
+
+super();
+this.state = {
+count: 0,
+
+};
+}
+
+render() {
+return (
+
+<button onClick={ () => {
+	this.setState ({count: this.state.count + 1});
+}}
+> 
+Count: {this.state.count}
+</button>
+
+);
+
+}
+
+}
+
+export default Counter;
+
